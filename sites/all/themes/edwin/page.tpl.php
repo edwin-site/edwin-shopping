c1263711f64074235a17afbf08dd1cb2a422c61d
Muhammad Ishfaq <ishfaqmch@gmail.com>
2013-04-10 22:20:53 +0500

    footer added

--- sites/all/themes/edwin/page.tpl.php
+++ sites/all/themes/edwin/page.tpl.php
@@ -77,7 +77,52 @@
 <?php endif; ?>
 
 
-  <div id="footer" class="prefix-1 suffix-1">
+  
+
+</div>
+
+<div id="footer" class="footer  clearfix">
+  <div class="inner-footer container-12 clearfix">
+    <div class="grid-5 suffix-1">
+      <h2>About Edwin Shopping</h2>
+      <p>Lorem ipsum dolor sit amet, suas gubergren an duo, prima iisque ei sed. Cum eu adipisci repudiare neglegentur. Vix alii prodesset necessitatibus eu, feugait evertitur no cum. Cum ne appetere maluisset, noster aperiam similique vim eu, sit id posse deterruisset. </p>
+      <div class="newsletter clearfix">
+        <input type="text" name="txt-newsletter" value="You can subscribe us for the updates here..." >
+        <input type="submit" name="btn-submit" value="join" />
+      </div>
+    </div>
+    <div class="grid-3">
+      <h2>Contact Us</h2>
+      <div class="contact-panel">
+        <ul>
+          <li class="phone">+923214076116</li>
+          <li class="email">ishfaqmch@gmail.com</li>
+          <li class="address">Main Boulevard, Hafeez Center, Gulberg III, Lahore</li>
+        </ul>
+      </div>
+    </div>
+    <div class="grid-3 social">
+      <h2>Get Social</h2>
+      <ul>
+        <li><a href="#"><img src="<?php print $base_url; ?>images/ico-rss.png" alt="Get our RSS Feed" title="Get our RSS Feed" width="31" height="32" /></a></li>
+        <li><a href="#"><img src="images/ico-facebook.png" alt="See us on Facebook" title="See us on Facebook" width="31" height="32" /></a></li>
+        <li><a href="#"><img src="images/ico-twitter.png" alt="Follow Us on Twitter" title="Follow Us on Twitter" width="31" height="32" /></a></li>
+        <li><a href="#"><img src="images/ico-linkedin.png" alt="See our profile on Linked in" title="See our profile on Linked in" width="31" height="32" /></a></li>
+      </ul>
+    </div>
+    <div class="clear"></div>
+    <div class="grid-7 footer-links">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Experties</a></li>
+        <li><a href="#">Get a Free qoute</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">CONTACT US</a></li>
+      </ul>
+    </div>
+    <div class="grid-4 prefix-1 footer-links">Edwin Shopping. &copy; 2013-2015. All rights reserved.</div>
+  </div>
     <?php if ($page['footer']): ?>
       <div id="footer-region" class="region grid-14 clearfix">
         <?php print render($page['footer']); ?>
@@ -85,4 +130,3 @@
     <?php endif; ?>
   </div>
 
-</div>
